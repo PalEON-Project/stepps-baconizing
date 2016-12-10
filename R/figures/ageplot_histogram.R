6a01364b6d67c0d75be147fb96d6490bdba42139
Simon <simon.j.goring@gmail.com>
2016-12-10 10:41:11 -0800

    Moved figure 1 into a new file and cleaned up the duplication of age models due to multiple default age model types returned from Neotoma.

---
+++ R/figures/ageplot_histogram.R
@@ -0,0 +1,47 @@
+
+chronology_type_histogram <- function(meta.table) {
+  
+  meta.table_short <- meta.table %>% group_by(id) %>% filter(year == max(year) & !is.na(chron)) %>% unique()
+  
+  # TODO: Because there are defaults for each model type some of these records have multiple
+  # entries, for example, both calibrated and uncalibrated ages.  Need to pick the best.
+  
+  meta.table_short$chron <- factor(meta.table_short$chron,
+                                   levels = c("Calendar years BP",
+                                              "Varve years BP",
+                                              "Calibrated radiocarbon years BP",
+                                              "Radiocarbon years BP"))
+  
+  for (i in meta.table_short$id[duplicated(meta.table_short$id)]) {
+    sample <- meta.table_short$id == i
+    drop   <- which.max(meta.table_short$chron[sample])
+    meta.table_short$chron[sample][drop] <- NA
+  }
+  
+  meta.table_short <- meta.table_short %>% na.omit
+  
+  plot_out <- ggplot(na.omit(meta.table_short), aes(x = year, fill = chron)) + 
+    geom_histogram(binwidth = 1) +
+    scale_x_continuous(limits = c(1948, 2014), expand = c(0,0)) + 
+    scale_y_continuous(limits = c(0, 30), expand = c(0,0)) + 
+    scale_fill_viridis(guide = guide_legend(title = "Chronology"), 
+                      discrete = TRUE) +
+    xlab('Publication Year') + 
+    ylab('Core Count') +
+    theme_bw() +
+    theme(axis.title.x = element_text(family = 'serif', 
+                                      face = 'bold.italic', 
+                                      size = 18),
+          axis.title.y = element_text(family = 'serif', 
+                                      face = 'bold.italic', 
+                                      size = 18),
+          axis.ticks = element_blank(),
+          axis.text.x = element_text(family = 'serif', 
+                                     face = 'italic', 
+                                     size = 14),
+          axis.text.y = element_text(family = 'serif', 
+                                     face = 'italic', 
+                                     size = 14))
+  return(plot_out)
+
+}
