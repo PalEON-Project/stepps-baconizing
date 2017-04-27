d62112620ffb9b91c55e1e18a876d7ee533cf995
SimonGoring <simon.j.goring@gmail.com>
2017-04-26 21:46:05 -0700

    Added theme to the thickness plot & added axis titles.

--- R/thick_model.R
+++ R/thick_model.R
@@ -1,10 +1,26 @@
 allan_thick <- function() {
-  thickness <- read.csv('data/pollen_meta_v1.0.csv')
+  thickness <- readr::read_csv('data/pollen_meta_v1.0.csv')
 
   thick_model <- ggplot(aes(x = thick, y = pol_age_max), data = thickness) + 
     geom_point() + 
     geom_smooth() +
-    theme_bw()
+    theme_bw() +
+    xlab("Best-Fit Model Section Thickness") +
+    ylab("Maximum Core Age") +
+    theme(axis.title.x = element_text(family = 'serif', 
+                                      face = 'bold.italic', 
+                                      size = 18),
+          legend.position = 'none',
+          axis.title.y = element_text(family = 'serif', 
+                                      face = 'bold.italic', 
+                                      size = 18),
+          axis.ticks = element_blank(),
+          axis.text.x = element_text(family = 'serif', 
+                                     face = 'italic', 
+                                     size = 14),
+          axis.text.y = element_text(family = 'serif', 
+                                     face = 'italic', 
+                                     size = 14))
   
   model <- gam(pol_age_max ~ s(thick, k = 3), data = thickness)
   
